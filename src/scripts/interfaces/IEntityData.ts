21d3565686a2b0a789ea29f7239010640074e321
Brian Kuan <bkuan72@gmail.com>
2022-10-18 22:15:20 +1300

    interface

---
+++ src/scripts/interfaces/IEntityData.ts
@@ -0,0 +1,10 @@
+import { BruceInfo, EntityType } from "bruce-maps-widget";
+
+
+export interface IEntityData
+{
+    record: BruceInfo;
+    type: EntityType;
+    photoUrl: string;
+    name: string;
+}
